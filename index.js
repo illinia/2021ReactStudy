67d2a3d90864f79751b344eccade5de623b6d4ab
illinia <67290425+illinia@users.noreply.github.com>
2021-04-03 13:30:45 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://test:12341234@boilerplate.ruzfr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 테스트')
 })
 
+app.get('/', (req, res) => res.send("Hello World!"))
 
+app.post('/register', (req, res) => {
+  // 회원가입 할때 필요한 정보들을 client에서 가져오면
+  // 데이터베이스에 넣어준다.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
