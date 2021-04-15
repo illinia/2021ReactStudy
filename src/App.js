b4e02fca16cc4df75df5b5eb0d2f0f6822b48e43
illinia <67290425+illinia@users.noreply.github.com>
2021-04-15 23:58:05 +0900

    news-viewer study

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import React, { useState } from 'react';
-import NewsList from './components/NewsList';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=628feb52906a47e2bdfff2ecab2c80eb',
-      );
-      setData(response.data);
-    } catch(e) {
-      console.log(e);
-    }
-  }
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
-    </div>
-  )
+  return <Route path="/:category?" component={NewsPage} />;
 }
 
 export default App;
